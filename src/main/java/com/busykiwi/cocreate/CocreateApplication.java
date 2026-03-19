8be8cb110e8886b8e1238dec590d92627879b09c
thebusykiwi <thebusykiwi@gmail.com>
2026-03-19 19:26:37 +0000

    chore: initial commit

---
+++ src/main/java/com/busykiwi/cocreate/CocreateApplication.java
@@ -0,0 +1,13 @@
+package com.busykiwi.cocreate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CocreateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CocreateApplication.class, args);
+	}
+
+}
